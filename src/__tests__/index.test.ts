5736669096c63fefa06d377666cf24f06c7e498f
Ivan Atias <ivan.d.atias@gmail.com>
2023-05-21 06:53:01 -0400

    test: use color and styles codes for arrangements

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,12 @@
 import { expect, test, describe } from 'vitest'
 import { cl } from '../index'
-import { COLOR_CODES } from '../utils/codes'
+import { COLOR_CODES, FONT_CODES, DECORATION_CODES } from '../utils/codes'
 import type { Color } from '../types'
+
+const { yellow, brightGreen } = COLOR_CODES
+const { bold, italic } = FONT_CODES
+const { underline, strikethrough } = DECORATION_CODES
+const STR = 'Text'
 
 describe('color-logs', () => {
   test('properties are named after supported colors', () => {
@@ -13,38 +18,41 @@ describe('color-logs', () => {
 
   test('properties values are functions that return strings', () => {
     expect(typeof cl.red).toBe('function')
-    expect(typeof cl.red('Text')).toBe('string')
+    expect(typeof cl.red(STR)).toBe('string')
   })
 
   test('returns the expected color string', () => {
-    const yellowText = '\x1b[38;5;3mText\x1b[38;5;0m'
+    const yellowText = `\x1b[38;5;${yellow[0]}m${STR}\x1b[38;5;${yellow[1]}m`
 
-    expect(cl.yellow('Text')).toBe(yellowText)
+    expect(cl.yellow(STR)).toBe(yellowText)
   })
 
   test('returns the expected color string with font and decoration variants', () => {
-    const text = '\x1b[3m\x1b[4m\x1b[38;5;82mText\x1b[38;5;0m\x1b[0m\x1b[0m' // Italic, underlined, bright green text
+    // const text = '\x1b[3m\x1b[4m\x1b[38;5;82mText\x1b[38;5;0m\x1b[0m\x1b[0m'  Italic, underlined, bright green text
+    const text = `\x1b[${italic[0]}m\x1b[${underline[0]}m\x1b[38;5;${brightGreen[0]}m${STR}\x1b[38;5;${brightGreen[1]}m\x1b[${italic[1]}m\x1b[${underline[1]}m`
 
     expect(
-      cl.brightGreen('Text', { font: 'italic', decoration: 'underline' })
+      cl.brightGreen(STR, { font: 'italic', decoration: 'underline' })
     ).toBe(text)
   })
 
   test('returns the expected color string if font or decoration variant is missing', () => {
-    const strikeThroughText = '\x1b[9m\x1b[38;5;82mText\x1b[38;5;0m\x1b[0m' // strikethrough, bright green text
-    const boldText = '\x1b[1m\x1b[38;5;82mText\x1b[38;5;0m\x1b[0m' // Bold, bright green text
+    // strikethrough, bright green text
+    const strikeThroughText = `\x1b[${strikethrough[0]}m\x1b[38;5;${brightGreen[0]}m${STR}\x1b[38;5;${brightGreen[1]}m\x1b[${strikethrough[1]}m`
+    // Bold, bright green text
+    const boldText = `\x1b[${bold[0]}m\x1b[38;5;${brightGreen[0]}m${STR}\x1b[38;5;${brightGreen[1]}m\x1b[${bold[1]}m`
 
-    expect(cl.brightGreen('Text', { decoration: 'strikethrough' })).toBe(
+    expect(cl.brightGreen(STR, { decoration: 'strikethrough' })).toBe(
       strikeThroughText
     )
-    expect(cl.brightGreen('Text', { font: 'bold' })).toBe(boldText)
+    expect(cl.brightGreen(STR, { font: 'bold' })).toBe(boldText)
   })
 
   test('returns the expected color string if passed an array of font styles', () => {
-    const boldItalicText =
-      '\x1b[1m\x1b[3m\x1b[38;5;82mText\x1b[38;5;0m\x1b[0m\x1b[0m' // Bold, italic bright green text
+    // Bold, italic bright green text
+    const boldItalicText = `\x1b[${bold[0]}m\x1b[${italic[0]}m\x1b[38;5;${brightGreen[0]}m${STR}\x1b[38;5;${brightGreen[1]}m\x1b[${bold[1]}m\x1b[${italic[1]}m`
 
-    expect(cl.brightGreen('Text', { font: ['bold', 'italic'] })).toBe(
+    expect(cl.brightGreen(STR, { font: ['bold', 'italic'] })).toBe(
       boldItalicText
     )
   })
@@ -52,7 +60,9 @@ describe('color-logs', () => {
   test('should not throw and use default values if invalid font or decoration is passed', () => {
     expect(
       // @ts-ignore
-      cl.brightGreen('Text', { font: 'invalid', decoration: '' })
-    ).toBe('\x1B[0m\x1B[0m\x1b[38;5;82mText\x1b[38;5;0m\x1B[0m\x1B[0m')
+      cl.brightGreen(STR, { font: 'invalid', decoration: '' })
+    ).toBe(
+      `\x1b[0m\x1b[0m\x1b[38;5;${brightGreen[0]}m${STR}\x1b[38;5;${brightGreen[1]}m\x1b[0m\x1b[0m`
+    )
   })
 })
